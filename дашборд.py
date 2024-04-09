e48183e557c0470274ff6dd7a716394bbd1526e7
Lesha22813 <31177669+Lesha22813@users.noreply.github.com>
2024-04-09 23:34:55 +1000

    Update дашборд.py

--- дашборд.py
+++ дашборд.py
@@ -14,6 +14,16 @@ app.layout = html.Div([
     html.H1(children='Информация о финансовых данных'),
 
     dcc.Dropdown(
+        id='period-dropdown',
+        options=[
+            {'label': 'Месяц', 'value': 'Месяц'},
+            {'label': 'Квартал', 'value': 'Квартал'},
+            {'label': 'Год', 'value': 'Год'}
+        ],
+        value='Месяц'
+    ),
+
+    dcc.Dropdown(
         id='dropdown',
         options=[
             {'label': 'Дата', 'value': 'Дата'},
@@ -30,7 +40,17 @@ app.layout = html.Div([
 
     dcc.Graph(id='graph'),
 
-    dcc.Graph(id='scatter-plot')
+    dcc.Graph(id='scatter-plot'),
+
+    dcc.Graph(id='pie-chart'),
+
+    dcc.Graph(id='histogram'),
+
+    dash_table.DataTable(
+        id='financial-metrics',
+        columns=[{'name': metric, 'id': metric} for metric in ['Прибыль', 'Выручка', 'Расходы']],  # Необходимо указать необходимые ключевые показатели
+        data=[]  # Начальные данные для таблицы
+    )
 ])
 
 @app.callback(
@@ -38,7 +58,7 @@ app.layout = html.Div([
     [Input('dropdown', 'value')]
 )
 def update_graph(selected_value):
-    fig = px.line(df, x='Дата', y=selected_value, title=f'График {selected_value} по времени')
+    fig = px.line(df, x='Дата', y='Сумма', title='Динамика доходов и расходов')
     return fig
 
 @app.callback(
@@ -46,8 +66,40 @@ def update_graph(selected_value):
     [Input('dropdown', 'value')]
 )
 def update_scatter_plot(selected_value):
-    scatter_fig = px.scatter(df, x='Карта', y=selected_value, title='График рассеяния')
+    scatter_fig = px.scatter(df, x='Дата', y='Сумма', title='График рассеяния')
     return scatter_fig
+
+@app.callback(
+    Output('pie-chart', 'figure'),
+    [Input('dropdown', 'value')]
+)
+def update_pie_chart(selected_value):
+    pie_fig = px.pie(df, names='Категория', values='Сумма', title='Структура расходов по категориям')
+    return pie_fig
+
+@app.callback(
+    Output('histogram', 'figure'),
+    [Input('dropdown', 'value')]
+)
+def update_histogram(selected_value):
+    hist_fig = px.histogram(df, x='Дата', y='Сумма', title='Анализ прибыли и ее распределения')
+    return hist_fig
+
+@app.callback(
+    Output('financial-metrics', 'data'),
+    [Input('dropdown', 'value')]
+)
+def update_financial_metrics(selected_value):
+    # Здесь вам нужно написать логику для расчета ключевых финансовых показателей и предоставления их в виде данных для таблицы
+    # Ниже приведен пример с использованием фиктивных значений для демонстрации
+    data = [
+        {
+            'Прибыль': 10000,
+            'Выручка': 30000,
+            'Расходы': 20000
+        }
+    ]
+    return data
 
 if __name__ == '__main__':
     app.run_server(debug=True)
